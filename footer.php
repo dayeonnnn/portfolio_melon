849b562dcccd7d69adf15eb75db6748b3f99f7f3
dayeon <tjdekdus@naver.com>
2023-09-06 18:43:33 +0900

    0906

--- footer.php
+++ footer.php
@@ -1,49 +1,49 @@
 <footer>
-                    <div class="footer_bg">
-                        <div class="logo">
-                            <h1><a href="./index.html"><img src="./images/logo.png" alt="Melon"></a></h1>
-                            <p>경기도 성남시 분당구 판교역로 221<br>대표이사 : 이진수, 김성수</p>
-                            <div class="call">
-                                <i class="fa-solid fa-phone"></i>1566-7727 (평일 09:00-18:00, 유료)
-                            </div>
-                            <div class="mail">
-                                <i class="fa-solid fa-envelope"></i>melon_info@kakaoent.com
-                            </div>
-                        </div>
-                        <div class="infor">
-                            <div class="infor_1">
-                                <p>Information</p>
-                                <a href="#">About Us</a>
-                                <a href="#">More Search</a>
-                                <a href="#">Blog</a>
-                                <a href="#">Testimonials</a>
-                                <a href="#">Events</a>
-                            </div>
-                            <div class="infor_2">
-                                <p>Helpful Link</p>
-                                <a href="#">Services</a>
-                                <a href="#">Supports</a>
-                                <a href="#">Terms & Condition</a>
-                                <a href="#">Privacy Policy</a>
-                            </div>
-                        </div>
-                        <div class="right">
-                            <div class="mail">
-                                <p>Subscribe More Info</p>
-                                <form action="#" method="get">
-                                    <fieldset>
-                                        <legend class="skip">메일주소</legend>
-                                        <i class="fa-solid fa-envelope"></i>
-                                        <input type="text" placeholder="Entet your Email">
-                                    </fieldset>
-                                    <button type="button">Subscribe</button>
-                                </form>
-                            </div>
-                            <div class="sns">
-                                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
-                                <a href="#"><i class="fa-brands fa-instagram"></i></a>
-                                <a href="#"><i class="fa-brands fa-twitter"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
+    <div class="footer_bg">
+        <div class="logo">
+            <h1><a href="./index.html"><img src="./images/logo.png" alt="Melon"></a></h1>
+            <p>경기도 성남시 분당구 판교역로 221<br>대표이사 : 이진수, 김성수</p>
+            <div class="call">
+                <i class="fa-solid fa-phone"></i>1566-7727 (평일 09:00-18:00, 유료)
+            </div>
+            <div class="mail">
+                <i class="fa-solid fa-envelope"></i>melon_info@kakaoent.com
+            </div>
+        </div>
+        <div class="infor">
+            <div class="infor_1">
+                <p>Information</p>
+                <a href="#">About Us</a>
+                <a href="#">More Search</a>
+                <a href="#">Blog</a>
+                <a href="#">Testimonials</a>
+                <a href="#">Events</a>
+            </div>
+            <div class="infor_2">
+                <p>Helpful Link</p>
+                <a href="#">Services</a>
+                <a href="#">Supports</a>
+                <a href="#">Terms & Condition</a>
+                <a href="#">Privacy Policy</a>
+            </div>
+        </div>
+        <div class="right">
+            <div class="mail">
+                <p>Subscribe More Info</p>
+                <form action="#" method="get">
+                    <fieldset>
+                        <legend class="skip">메일주소</legend>
+                        <i class="fa-solid fa-envelope"></i>
+                        <input type="text" placeholder="Entet your Email">
+                    </fieldset>
+                    <button type="button">Subscribe</button>
+                </form>
+            </div>
+            <div class="sns">
+                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-twitter"></i></a>
+            </div>
+        </div>
+    </div>
+</footer>
